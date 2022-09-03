756cd6eb114c3e727f68175809f25e1e2355cac9
Leophen <leophen@foxmail.com>
2022-09-03 22:59:32 +0800

    feat: mockjs use

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { mock, Random } from 'mockjs';
 
 type Data = {
-  name: string
-}
+  name: string,
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    mock({
+      // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+      'list|1-10': [
+        {
+          'name|1': [Random.cname(), Random.cname(), Random.cname()],
+          birthday: Random.date('yyyy-MM-dd'),
+          address: Random.county(true),
+        },
+      ],
+    })
+  );
 }
