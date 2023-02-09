95af9dd0f5b660e45f3ec27b76c7736a0777785d
Leophen <Leophen@foxmail.com>
2023-02-09 20:55:44 +0800

    chore: 修改接口访问地址

--- front-end-project/src/http/config/index.ts
+++ front-end-project/src/http/config/index.ts
@@ -1,6 +1,6 @@
 const serverConfig = {
-  // baseURL: 'http://localhost:443',
-  baseURL: 'http://43.136.107.135:443',
+  // baseURL: 'http://localhost:7676',
+  baseURL: 'https://api.leophen.top',
   useTokenAuthorization: true // 是否开启 token 认证
 }
 
