e83e7615484616aaf633f961c9bb1559af80ac31
Leophen <Leophen@foxmail.com>
2023-02-10 15:09:04 +0800

    chore: cors test

--- server-project-koa/app.js
+++ server-project-koa/app.js
@@ -17,11 +17,10 @@ const user = require('./routes/user')
 
 // CORS 跨域
 app.use(cors({
-  // origin: (ctx) => {
-  //   const allowCors = ['http://localhost:8001', 'https://i-whimsy.leophen.top'];
-  //   return allowCors.indexOf(ctx.header.origin) > -1 ? ctx.header.origin : '';
-  // },
-  origin: 'https://api.leophen.top/',
+  origin: (ctx) => {
+    const allowCors = ['http://localhost:8001'];
+    return allowCors.indexOf(ctx.header.origin) > -1 ? ctx.header.origin : '';
+  },
   allowedHeaders: 'Content-Type',
   credentials: true,
 }))
