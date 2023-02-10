e1fd23f9bb7a6da875775a61b463e7dc62fc7da7
Leophen <Leophen@foxmail.com>
2023-02-10 10:12:36 +0800

    fix: 请求接口

--- front-end-project/src/http/config/index.ts
+++ front-end-project/src/http/config/index.ts
@@ -1,6 +1,6 @@
 const serverConfig = {
   // baseURL: 'http://localhost:7676',
-  baseURL: 'https://api.leophen.top:7676',
+  baseURL: 'https://api.leophen.top',
   useTokenAuthorization: true // 是否开启 token 认证
 }
 
