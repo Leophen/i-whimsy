7b39a6827eeee1ebee10bc3f8f9efe231f7417fe
Leophen <leophen@foxmail.com>
2023-02-18 11:28:52 +0800

    chore: 线上请求地址测试

--- front-end-project/src/http/config/index.ts
+++ front-end-project/src/http/config/index.ts
@@ -1,7 +1,9 @@
 const ENV = process.env.NODE_ENV === 'development' ? 'dev' : 'prd';
+const ADMIN = 'http://43.136.107.135:7676';
+// const ADMIN = 'https://api.leophen.top';
 
 const serverConfig = {
-  baseURL: ENV === 'dev' ? 'http://localhost:7676' : 'https://api.leophen.top',
+  baseURL: ENV === 'dev' ? 'http://localhost:7676' : ADMIN,
   useTokenAuthorization: true, // 是否开启 token 认证
 };
 
